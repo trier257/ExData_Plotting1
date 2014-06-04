f4cae80ff8a738337fa09f528d12b8db981ecd76
Trier257 <trier257@yahoo.com>
2014-06-04 18:34:39 -0400

    Updated plot 2 background to white

--- plot2.R
+++ plot2.R
@@ -1,9 +1,9 @@
 # Read in the data. (The data setup here is the same as in plot1.R.)
 # This code requires the zip file be downloaded and unzipped to the current directory.
-# Read in the data. It's big but not so large that it won't fit in memory.
+# The data's big but not so large that it won't fit in memory.
 allData <- read.table("household_power_consumption.txt", sep=";", header=TRUE, stringsAsFactors=FALSE, na.strings="?")
 
-# The assignment only deals with the data for Feb 1, 2007 to end of Feb 2, 2007..
+# The assignment only deals with the data for Feb 1, 2007 to end of Feb 2, 2007.
 # Need to convert data's dates, then subset with them.
 
 # Convert character date and time to a date-time value using strptime.
@@ -19,8 +19,8 @@ bDate <- strptime("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S")
 eDate <- strptime("03/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S")
 gapData <- subset(allData, DateTime >= bDate & DateTime < eDate, select=c(3,10))
 
-# Open the PNG device with the required 480x480
-png(file="plot2.png", bg="transparent", width=480, height=480)
+# Open the PNG device with the required 480x480.
+png(file="plot2.png", bg="white", width=480, height=480)
 # Create a line plot to match the assignment's plot 2.
 # Note this plot has no title or x-axis label.
 with(gapData, plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
